d8f56cce064c370d7bdc9b0590db0723c0016b14
Vali <Vali_Schagerl@web.de>
2016-10-10 23:19:47 +0200

    change in dialogmanager

--- core/src/com/mygdx/game/overworldObjects/Dialog/DialogManager.java
+++ core/src/com/mygdx/game/overworldObjects/Dialog/DialogManager.java
@@ -154,7 +154,7 @@ public class DialogManager {
         Dialog dialog15 = new Dialog(textArray, avatarArray);
 
         textArray = new Array<String>();
-        textArray.addAll("Harry? The Tiger has landed","Nicely done Mr Thirst. By the way i found some really well paid jobs for you by Gen Corp");
+        textArray.addAll("Harry? The Tiger has landed","Nicely done Mr Thirst. By the way I found some really well paid jobs for you by Gen Corp");
         avatarArray = new Array<DialogManager.Avatar>();
         avatarArray.addAll(Avatar.GIRL_NORMAL, Avatar.JASON_HAPPY, Avatar.GIRL_ANGRY, Avatar.GIRL_HAPPY);
         Dialog dialog20 = new Dialog(textArray, avatarArray);
